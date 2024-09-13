e9944f435acf97c665d6d38c5933390b832de0ec
Animesh Bindal <98091040+Animesh-Bindal@users.noreply.github.com>
2024-09-13 22:51:08 +0530

    customer and footer page

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,69 @@
 import React from 'react'
+import "../Styles/footer.scss";
+import logo from '../logo2.png';
+import loc from "../footericons/location.png";
+import call from "../footericons/call.png";
+import linkedin from "../footericons/in.png";
+import youtube from "../footericons/yt.png";
+import inst from "../footericons/inst.png";
 
 function Footer() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='footer'>
+            <div className="main">
+                <div className='topcont'>
+                    <h2 className='top'>Manage your travel & expenses the smart way!</h2>
+                </div>
+                <div className="formdetails">
+                    <div className="form">
+                        <input type="text" placeholder="Name" className='name' />
+                        <input type="email" placeholder="Official email ID" className='email' />
+                        <button>Start trial</button>
+                    </div>
+                    <p className="trial-details">
+                        30 Days free trial • Upto 10 users
+                    </p>
+                </div>
+            </div>
+
+            <div className="foot">
+                <div className="foottop">
+                    <div className='lgo'>
+                        <img src={logo} alt="" className='logo' />
+                    </div>
+                    <div className='rightt'>
+                        <div className="addr">
+                            <img src={loc} alt="" />
+                            <p>Address</p>
+                        </div>
+                        <div className="ph">
+                            <img src={call} alt="" />
+                            <p>+91 1234567890</p>
+                        </div>
+                        <div className="bottomm">
+                            <p>Social Media</p>
+                            <img src={linkedin} alt="" />
+                            <img src={youtube} alt="" />
+                            <img src={inst} alt="" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="footbot">
+                    <div className="footnav">
+                        <a href="#blogs">Blogs</a>
+                        <a href="#features">Features</a>
+                        <a href="#pricing">Pricing</a>
+                        <a href="#integrations">Integrations</a>
+                    </div>
+                    <div className="copyr">
+                        <p>Copyright &copy; 2024 . ExpenseBook</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default Footer
--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import "../Styles/Testimonials.scss";
+import img1 from "../Integicons/img1.png";
+import img2 from "../Integicons/img2.png";
+import img3 from "../Integicons/img3.png";
 
 function Testimonials() {
+  const testimonials = [
+    {
+      name: "Rohit Mehra",
+      title: "CFO, XYZ",
+      quote: "ExpenseBook has truly streamlined our travel setup. No more emails and delays in booking. No more unknown overruns in expense. Everything is trackable, and the configurability they offer is outstanding.",
+      imagee: img1
+    },
+    {
+      name: "Alan Turing",
+      title: "CFO, XYZ",
+      quote: "ExpenseBook has truly streamlined our travel setup. No more emails and delays in booking. No more unknown overruns in expense. Everything is trackable, and the configurability they offer is outstanding.",
+      imagee: img2
+    },
+    {
+      name: "Mark Jacob",
+      title: "CEO, XYZ",
+      quote: "ExpenseBook has truly streamlined our travel setup. No more emails and delays in booking. No more unknown overruns in expense. Everything is trackable, and the configurability they offer is outstanding.",
+      imagee: img3
+    }
+  ];
   return (
-    <div>
-      
+    <div className="section-testimonials">
+      <p className='intp'>Our customers love us</p>
+      <div className="testimonial-cards" >
+        {testimonials.map((testimonial, index) => (
+          <div key={index} className={`testimonial-card ${testimonial.name === "Rohit Mehra" ? 'orange' : testimonial.name === "Alan Turing" ? 'green' : testimonial.name === "Mark Jacob" ? 'bluee' : ''}`}>
+            <p>{testimonial.quote}</p>
+            <div className="bot">
+              <img src={testimonial.imagee} alt={`${testimonial.name} picc`} />
+              <div className="des">
+                <h4>{testimonial.name}</h4>
+                <p className="title">{testimonial.title}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
