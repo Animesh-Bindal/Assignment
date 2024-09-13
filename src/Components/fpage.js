cbe519b674795f9430f5ca90261e252699af9299
Animesh Bindal <98091040+Animesh-Bindal@users.noreply.github.com>
2024-09-13 20:21:55 +0530

    some additions

--- src/Components/fpage.js
+++ src/Components/fpage.js
@@ -1,4 +1,16 @@
 import React from 'react'
+import '../Styles/fpage.scss';
+import logo from "../hotjar.png";
+
+import img1 from "../featureicons/1.png";
+import img2 from "../featureicons/2.png";
+import img3 from "../featureicons/3.png";
+import img4 from "../featureicons/4.png";
+import img5 from "../featureicons/5.png";
+import img6 from "../featureicons/6.png";
+import img7 from "../featureicons/7.png";
+import img8 from "../featureicons/8.png";
+import img9 from "../featureicons/9.png";
 
 function fpage() {
   return (
